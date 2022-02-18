198389b97c313de5dc33ce7ca8d5ab0f5a4a9c5d
gjesionowski <gregory.a.jesionowski@gmail.com>
2022-02-18 15:02:35 -0800

    fine tunes graphing terminal output and graph design

--- shell/datclean.sh
+++ shell/datclean.sh
@@ -18,12 +18,10 @@ echo "Data copy from switch to Ansible server finished. If connection has been r
 awk 'BEGIN{RS="Switch Port Counters"}{print $242"/"$258","$65","$61}' /home/pi/testing/data/port$PORT.dat > /home/pi/testing/data/$check12-port$PORT.clean
 echo "Data formatting finished"
 cd /home/pi/testing/shell
-pwd
 
 # Gets the most recent data file name
 currentdata=$(ls -t ~/testing/data/ | grep clean | head -n1)
 echo $currentdata
-pwd
 
 # Graphs the most recent data file, saves with switch/port/time
 gnuplot -p <<EOF
@@ -31,6 +29,7 @@ set title "FCS Errors and Total Packets\n{/*0.6 Switch $SWITCH - Port $PORT - fr
 set terminal pngcairo dashed
 set style data lines
 set output '/home/pi/testing/backups/switch$SWITCH-port$PORT-$check12-graph.png'
+FACTOR=0.000001
 set datafile sep ","
 set xlabel "Elapsed Time (minutes)"
 set ylabel "Total Packets (millions)"
@@ -38,22 +37,20 @@ set y2label "Total FCS Errors"
 set xtics 60 rotate by -30 offset -0.5
 set mxtics 4
 set y2tics 10
-set my2tics
+set my2tics 10
 set y2range [0:*]
 set ytics nomirror
 #set mytics 2
 set style line 8 lc rgb "gray50" lw 0.5 lt 1
 set style line 4 lc rgb "gray80" lw 0.5 lt 1
 set grid mxtics xtics ls 8, ls 4
-FACTOR=0.000001
 plot \
-'/home/pi/testing/data/$currentdata' using 0:2 t "FCS Errors" w lines axes x1y2, \
+'/home/pi/testing/data/$currentdata' using 0:2 t "FCS Errors" w steps axes x1y2, \
 '/home/pi/testing/data/$currentdata' using 0:(\$3*FACTOR) t "Packets" w lines
 EOF
 echo "Data graphing finished"
 
 # Opens the finished graph, or most recent graph in directory
 cd /home/pi/testing/backups
-pwd
 xdg-open $(ls -t /home/pi/testing/backups | grep graph.png | head -n1)
 echo "Graph opening finished"
