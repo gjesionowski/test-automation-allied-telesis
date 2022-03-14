90286aaecb7c5ca0d2aa6cba31a9cb99d1969ba2
gjesionowski <gregory.a.jesionowski@gmail.com>
2022-03-14 13:49:18 -0700

    Functional chariot testing at user input speed

---
+++ powershell/start-test-1000-122-123.ps1
@@ -0,0 +1,3 @@
+$CharTest = "C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1"
+
+./$CharTest
--- testconfig.sh
+++ testconfig.sh
@@ -3,21 +3,36 @@
 # Stream 3 into 1 ;; Then stream 2 into 1 + 1 into 3 ;; Gives stderror and stdout
 exec 3>&1;
 uut=$(dialog --title "Test Configuration" --inputbox "Enter the name of the device being tested:" 0 0 2>&1 1>&3);
-host1=$(dialog --title "Test Configuration" --inputbox "Enter the hostname of the first endpoint:" 0 0 2>&1 1>&3);
-host2=$(dialog --title "Test Configuration" --inputbox "Enter the hostname of the other endpoint:" 0 0 2>&1 1>&3);
-duration=$(dialog --title "Test Configuration" --inputbox "Enter the duration (in minutes) of the test: [24hrs = 1440]" 0 0 2>&1 1>&3);
-speed=$(dialog --title "Test Configuration" --radiolist "Select (with Space) the test speed and press Enter [Default: Auto-negotiate]:" 20 60 7 2>&1 1>&3 \
+host1=$(dialog --title "$uut Test Configuration: Host A" --radiolist "Select (with Space) the OS family of Test PC 1:" 0 0 2>&1 1>&3 \
+Windows "(All versions)" on \
+Linux "(All versions)" off \
+);
+host2=$(dialog --title "$uut Test Configuration: Host B" --radiolist "Select (with Space) the OS family of Test PC 2:" 0 0 2>&1 1>&3 \
+Windows "(All versions)" on \
+Linux "(All versions)" off \
+);
+duration=$(dialog --title "$uut Test Configuration: Test Duration" --radiolist "Select (with Space) the test duration (in minutes) of the test: [Default: 15 Minutes]" 20 60 7 2>&1 1>&3 \
+1 "Minute" off \
+15 "Minutes" on \
+30 "Minutes" off  \
+60 "Minutes" off \
+120 "Minutes" off \
+720 "Minutes (12 hours)" off \
+1440 "Minutes (1 day)" off \
+2880 "Minutes (2 days)" off \
+5760 "Minutes (4 days)" off );
+speed=$(dialog --title "$uut Test Configuration: Speed Setting" --radiolist "Select (with Space) the test speed and press Enter [Default: Auto-negotiate]:" 20 60 7 2>&1 1>&3 \
 100 "Mbps, Full Duplex" off \
 1000 "Mbps, Full Duplex" off \
 10000 "Mbps, Full Duplex" off  \
 auto "Auto-negotiate" on );
-octet=$(dialog --title "Test Configuration" --inputbox "Enter the last octet of the switch IP [i.e. xx in 192.162.1.xx]:" 0 0 2>&1 1>&3);
-port1=$(dialog --title "Test Configuration" --inputbox "Enter the first switch port to monitor [i.e. xx in port1.0.xx]:" 0 0 2>&1 1>&3);
-cable1=$(dialog --inputbox "Enter the type of cable connected to first switch port:" 0 0 2>&1 1>&3);
-cable1length=$(dialog --inputbox "Enter the length (meters) of the cable connected to first switch port:" 0 0 2>&1 1>&3);
-port2=$(dialog --title "Test Configuration" --inputbox "Enter the second switch port to monitor [i.e. xx in port1.0.xx]:" 0 0 2>&1 1>&3);
-cable2=$(dialog --inputbox "Enter the type of cable connected to second switch port" 0 0 2>&1 1>&3);
-cable2length=$(dialog --inputbox "Enter the length (meters) of the cable connected to second switch port:" 0 0 2>&1 1>&3);
+octet=$(dialog --title "$uut Test Configuration: Switch IP" --inputbox "Enter the last octet of Switch IP [i.e. xx in 192.162.1.xx]:" 0 0 2>&1 1>&3);
+port1=$(dialog --title "$uut Test Configuration: Port IDs" --inputbox "Enter the first switch port to monitor [i.e. x in port1.0.x]:" 0 0 2>&1 1>&3);
+#cable1=$(dialog --inputbox "Type of cable connected to port1.0.$port1:" 0 0 2>&1 1>&3);
+#cable1length=$(dialog --inputbox "Length (meters) of the cable connected to port1.0.$port1:" 0 0 2>&1 1>&3);
+port2=$(dialog --title "$uut Test Configuration: Port IDs" --inputbox "Enter the second switch port to monitor [i.e. x in port1.0.x]:" 0 0 2>&1 1>&3);
+#cable2=$(dialog --inputbox "Type of cable connected to port1.0.$port2" 0 0 2>&1 1>&3);
+#cable2length=$(dialog --inputbox "Length (meters) of the cable connected to port1.0.$port2:" 0 0 2>&1 1>&3);
 exec 3>&-;
 
 # Time/date used to create unique filenames for now
@@ -47,7 +62,7 @@ echo "Previous Configuration data saved to $backupdir$filenamebackup";
 echo "Current Configuration data saved to $yamldir$filename";
 echo " ... "
 
-## Enter the main testing Ansible playbook. 
-## Multiple Passwords required for encryption and escalation
-## Ask permission for each step of the way
-ansible-playbook --ask-become-pass --ask-vault-pass -vvv --step ~/testing/yaml/test.yml
+## Enter the main testing Ansible playbook. This may be split up into multiple playbooks soon because of increasing complexity. 
+## Multiple Passwords required for encryption and escalation (--ask-become-pass & --ask-vault-pass)
+## Ask permission for each step of the way (--step)
+ansible-playbook --ask-become-pass --ask-vault-pass -vvv ~/testing/yaml/test.yml
