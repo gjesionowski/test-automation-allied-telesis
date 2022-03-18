3700987a18e2db7639e8ddf109982246d6d42235
gjesionowski <gregory.a.jesionowski@gmail.com>
2022-03-18 11:18:28 -0700

    remove port clearing before Jumbo Frame test, no port counters used here

---
+++ powershell/set-jumbo-enabled-on-allied-macs.ps1
@@ -0,0 +1,6 @@
+# EC-CD-6D or E0-1A-EA
+$CurrMac = get-netadapter | Where {$_.MacAddress -Like "E0-1A-EA*"} | Where {$_.Status -ne "Disabled"}
+$JumboSetting = "9014" # Jumbo frames enabled
+# Later, a ForEach ($adap in $CurrMac ........ )
+Set-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*" -RegistryValue $JumboSetting
+Get-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*"
