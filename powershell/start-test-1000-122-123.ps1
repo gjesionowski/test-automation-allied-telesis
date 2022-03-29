94cc2e42f8c790e77b7baaf13d844b6c0f79e6bb
gjesionowski <gregory.a.jesionowski@gmail.com>
2022-03-29 13:48:01 -0700

    removes use of assigned variable for better form

--- powershell/start-test-1000-122-123.ps1
+++ powershell/start-test-1000-122-123.ps1
@@ -1,3 +1,2 @@
-$CharTest = "C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1"
-
-./$CharTest
+# $CharTest = "C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1"; ./$CharTest
+./C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1
