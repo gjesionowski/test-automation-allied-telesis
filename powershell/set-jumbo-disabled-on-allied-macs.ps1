c8e89e4ade42b04e254e97c662f848bacec5cdea
gjesionowski <gregory.a.jesionowski@gmail.com>
2022-03-29 15:02:35 -0700

    cleans up jumbo frame powershell scripts, extends MAC finding

--- powershell/set-jumbo-disabled-on-allied-macs.ps1
+++ powershell/set-jumbo-disabled-on-allied-macs.ps1
@@ -1,6 +1,23 @@
 # EC-CD-6D or E0-1A-EA
-$CurrMac = get-netadapter | Where-Object {$_.MacAddress -Like "E0-1A-EA*"} | Where-Object {$_.Status -ne "Disabled"}
+# If this starts failing, consider that it used to look for non-disabled ports instead of just Up
 $JumboSetting = "1514" # Jumbo frames disabled
+
 # Later, a ForEach ($adap in $CurrMac ........ ) to support multi-port NICs
+$CurrMac = get-netadapter | Where-Object {$_.MacAddress -Like "E0-1A-EA*"} | Where-Object {$_.Status -Like "Up"}
+#If no matches found, don't try
+if ($null -ne $CurrMac)
+{
 Set-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*" -RegistryValue $JumboSetting
-Get-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*"
+Get-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*" # Display jumbo setting
+}
+
+$CurrMac = get-netadapter | Where-Object {$_.MacAddress -Like "EC-CD-6D*"} | Where-Object {$_.Status -Like "Up"}
+#If no matches found, don't try
+if ($null -ne $CurrMac)
+{
+Set-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*" -RegistryValue $JumboSetting
+Get-NetAdapterAdvancedProperty $CurrMac.Name -DisplayName "Jumbo*" # Display jumbo setting
+}
+# Clear var values
+$CurrMac = ""
+$JumboSetting = ""
--- powershell/start-test-1000-122-123.ps1
+++ powershell/start-test-1000-122-123.ps1
@@ -1,2 +1,2 @@
-# $CharTest = "C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1"; ./$CharTest
+# Original, delete this if running correctly with below... $CharTest = "C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1"; ./$CharTest
 ./C:\Users\Test\Desktop\ChariotScriptingFiles\Chariot1Gb122to123.ps1
