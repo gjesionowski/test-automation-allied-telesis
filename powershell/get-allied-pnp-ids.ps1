5e40256e6701c6fca9c9b0698460dcce0db815d6
gjesionowski <gregory.a.jesionowski@gmail.com>
2022-03-30 11:57:04 -0700

    adds pnp ID check for static PID and VID, should work for the majority of Allied NICs

---
+++ powershell/get-allied-pnp-ids.ps1
@@ -0,0 +1,5 @@
+﻿Get-PnpDevice -Class Net |
+Where-Object Status -Like "OK" | 
+Where-Object DeviceId -Like "*14e4*" | 
+Where-Object DeviceId -Like "*1656*" |
+Get-PnpDeviceProperty | Where-Object KeyName -Like "*HardwareIds*" | Format-Table DeviceId
